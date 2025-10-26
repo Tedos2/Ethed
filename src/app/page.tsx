93ea795a6b5c32e2faeca739e8396819c6bbdc57
Teddy Nogin <tedyno55@gmail.com>
2025-10-26 14:46:00 +0200

    ץ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Hero from "@/components/sections/Hero";
 import OrbitingSkills from "@/components/ui/orbiting-skills";
-import IntegrationsSection from "@/components/ui/integrations-section";
+import Demo from "@/components/ui/demo";
+import FAQ from "@/components/ui/faq";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <OrbitingSkills />
-      <IntegrationsSection />
+      <Demo />
+      <FAQ />
     </main>
   );
 }
---
+++ src/components/ui/faq.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React from "react";
+
+const App = () => {
+    const [openIndex, setOpenIndex] = React.useState(null);
+
+    const faqs = [
+        {
+            question: "How to use this component?",
+            answer: "To use this component, you need to import it in your project and use it in your JSX code. Here's an example of how to use it:",
+        },
+        {
+            question: "Are there any other components available?",
+            answer: "Yes, there are many other components available in this library. You can find them in the 'Components' section of the website.",
+        },
+        {
+            question: "Are components responsive?",
+            answer: "Yes, all components are responsive and can be used on different screen sizes.",
+        },
+        {
+            question: "Can I customize the components?",
+            answer: "Yes, you can customize the components by passing props to them. You can find more information about customizing components in the 'Customization' section of the website.",
+        },
+    ];
+    return (
+        <>
+            <style>{`
+                @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+                * {
+                    font-family: 'Poppins', sans-serif;
+                }
+            `}</style>
+            <div className="bg-[#0f0f0f] py-20">
+                <div className="max-w-xl mx-auto flex flex-col items-center justify-center px-4 md:px-0">
+                    <p className="text-[#FF7742] text-sm font-medium">FAQ's</p>
+                    <h1 className="text-3xl font-semibold text-center text-white">Looking for answer?</h1>
+                    <p className="text-sm text-gray-400 mt-2 pb-8 text-center">
+                        Ship Beautiful Frontends Without the Overhead — Customizable, Scalable and Developer-Friendly UI Components.
+                    </p>
+                    {faqs.map((faq, index) => (
+                        <div className="border-b border-white/20 py-4 cursor-pointer w-full" key={index} onClick={() => setOpenIndex(openIndex === index ? null : index)}>
+                            <div className="flex items-center justify-between">
+                                <h3 className="text-base font-medium text-white">
+                                    {faq.question}
+                                </h3>
+                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" className={`${openIndex === index ? "rotate-180" : ""} transition-all duration-500 ease-in-out flex-shrink-0`}>
+                                    <path d="m4.5 7.2 3.793 3.793a1 1 0 0 0 1.414 0L13.5 7.2" stroke="#FFFFFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </div>
+                            <p className={`text-sm text-gray-400 transition-all duration-500 ease-in-out max-w-md ${openIndex === index ? "opacity-100 max-h-[300px] translate-y-0 pt-4" : "opacity-0 max-h-0 -translate-y-2"}`} >
+                                {faq.answer}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default App;
